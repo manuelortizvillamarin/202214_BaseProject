6ace81b5ccf860652d94e06164bd25a4e2f427a6
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-14 18:51:38 -0500

    Create modules and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, StoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
