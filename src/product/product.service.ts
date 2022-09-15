0492cc611ac993230567f2786ff3073ccf6d83be
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-15 12:06:01 -0500

    Add CRUD operations for Product and Store entities

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { ProductEntity } from './product.entity';
 
@@ -10,5 +11,38 @@ export class ProductService {
         @InjectRepository(ProductEntity)
         private readonly productRepository: Repository<ProductEntity>
     ) { }
+
+    async findAll(): Promise<ProductEntity[]> {
+        return await this.productRepository.find({ relations: ["stores"] });
+    }
+
+    async findOne(id: string): Promise<ProductEntity> {
+        const product: ProductEntity = await this.productRepository.findOne({ where: { id }, relations: ["stores"] });
+        if (!product)
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+
+        return product;
+    }
+
+    async create(product: ProductEntity): Promise<ProductEntity> {
+        return await this.productRepository.save(product);
+    }
+
+    async update(id: string, product: ProductEntity): Promise<ProductEntity> {
+        const persistedProduct: ProductEntity = await this.productRepository.findOne({ where: { id } });
+        if (!persistedProduct)
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+
+        product.id = id;
+        return await this.productRepository.save(product);
+    }
+
+    async delete(id: string) {
+        const product: ProductEntity = await this.productRepository.findOne({ where: { id } });
+        if (!product)
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+
+        await this.productRepository.remove(product);
+    }
 
 }
