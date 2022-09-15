88e15743e3d6d2a9778ab1a217189640b17b01e2
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-15 18:37:54 -0500

    Add tests for the Product and Store services

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -45,4 +45,36 @@ describe('ProductService', () => {
     expect(products).toHaveLength(productsList.length);
   });
 
+  it('findOne should return a product by id', async () => {
+    const storedProduct: ProductEntity = productsList[0];
+    const product: ProductEntity = await service.findOne(storedProduct.id);
+    expect(product).not.toBeNull();
+    expect(product.name).toEqual(storedProduct.name)
+    expect(product.price).toEqual(storedProduct.price)
+    expect(product.type).toEqual(storedProduct.type)
+  });
+
+  it('findOne should throw an exception for an invalid product', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The product with the given id was not found")
+  });
+
+  it('create should return a new product', async () => {
+    const product: ProductEntity = {
+      id: "",
+      name: faker.company.name(),
+      price: faker.finance.amount(),
+      type: faker.lorem.word(),
+      stores: []
+    }
+
+    const newProduct: ProductEntity = await service.create(product);
+    expect(newProduct).not.toBeNull();
+
+    const storedProduct: ProductEntity = await repository.findOne({ where: { id: newProduct.id } })
+    expect(storedProduct).not.toBeNull();
+    expect(storedProduct.name).toEqual(newProduct.name)
+    expect(storedProduct.price).toEqual(newProduct.price)
+    expect(storedProduct.type).toEqual(newProduct.type)
+  });
+
 });
--- src/store/store.service.spec.ts
+++ src/store/store.service.spec.ts
@@ -46,4 +46,36 @@ describe('StoreService', () => {
     expect(stores).toHaveLength(storesList.length);
   });
 
+  it('findOne should return a store by id', async () => {
+    const storedStore: StoreEntity = storesList[0];
+    const store: StoreEntity = await service.findOne(storedStore.id);
+    expect(store).not.toBeNull();
+    expect(store.name).toEqual(storedStore.name)
+    expect(store.city).toEqual(storedStore.city)
+    expect(store.address).toEqual(storedStore.address)
+  });
+
+  it('findOne should throw an exception for an invalid store', async () => {
+    await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The store with the given id was not found")
+  });
+
+  it('create should return a new store', async () => {
+    const store: StoreEntity = {
+      id: "",
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress(),
+      products: []
+    }
+
+    const newStore: StoreEntity = await service.create(store);
+    expect(newStore).not.toBeNull();
+
+    const storedStore: StoreEntity = await repository.findOne({ where: { id: newStore.id } })
+    expect(storedStore).not.toBeNull();
+    expect(storedStore.name).toEqual(newStore.name)
+    expect(storedStore.city).toEqual(newStore.city)
+    expect(storedStore.address).toEqual(newStore.address)
+  });
+
 });
