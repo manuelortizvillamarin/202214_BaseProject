0c8dec81f6766ac56710dc5808b379f15c99a0d3
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-15 11:26:38 -0500

    Setup TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductEntity } from './product/product.entity';
 import { ProductModule } from './product/product.module';
+import { StoreEntity } from './store/store.entity';
 import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [ProductModule, StoreModule],
+  imports: [
+    ProductModule,
+    StoreModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'store',
+      entities: [ProductEntity, StoreEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
