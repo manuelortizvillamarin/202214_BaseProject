dce36fddd4633b7f487c4fd9d845038cd06dbef0
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-15 13:07:21 -0500

    Add tests for findAll in Product and Store services

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { ProductEntity } from './product.entity';
 import { ProductService } from './product.service';
+import { faker } from '@faker-js/faker';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let repository: Repository<ProductEntity>;
+  let productsList = [];
+
+  const seedDatabase = async () => {
+    repository.clear();
+    productsList = [];
+    for (let i = 0; i < 5; i++) {
+      const product: ProductEntity = await repository.save({
+        name: faker.company.name(),
+        price: faker.finance.amount(),
+        type: faker.lorem.word()
+      })
+      productsList.push(product);
+    }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [ProductService],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    repository = module.get<Repository<ProductEntity>>(getRepositoryToken(ProductEntity));
+    await seedDatabase();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('findAll should return all products', async () => {
+    const products: ProductEntity[] = await service.findAll();
+    expect(products).not.toBeNull();
+    expect(products).toHaveLength(productsList.length);
+  });
+
 });
--- src/store/store.service.spec.ts
+++ src/store/store.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { StoreEntity } from './store.entity';
 import { StoreService } from './store.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { faker } from '@faker-js/faker';
 
 describe('StoreService', () => {
   let service: StoreService;
+  let repository: Repository<StoreEntity>;
+  let storesList = [];
+
+  const seedDatabase = async () => {
+    repository.clear();
+    storesList = [];
+    for (let i = 0; i < 5; i++) {
+      const store: StoreEntity = await repository.save({
+        name: faker.company.name(),
+        city: faker.address.cityName(),
+        address: faker.address.streetAddress()
+      })
+      storesList.push(store);
+    }
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StoreService],
+      imports: [...TypeOrmTestingConfig()],
+      providers: [
+        StoreService],
     }).compile();
 
     service = module.get<StoreService>(StoreService);
+    repository = module.get<Repository<StoreEntity>>(getRepositoryToken(StoreEntity));
+    await seedDatabase();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('findAll should return all stores', async () => {
+    const stores: StoreEntity[] = await service.findAll();
+    expect(stores).not.toBeNull();
+    expect(stores).toHaveLength(storesList.length);
+  });
+
 });
