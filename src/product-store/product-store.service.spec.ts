0b499b30df8634d03f7ff5c37de04fa7b06befa9
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-16 18:06:04 -0500

    Add tests for findStoresFromProduct findStoreFromProduct updateStoresFromProduct deleteStoreFromProduct

--- src/product-store/product-store.service.spec.ts
+++ src/product-store/product-store.service.spec.ts
@@ -95,6 +95,15 @@ describe('ProductStoreService', () => {
     await expect(() => service.addStoreToProduct("0", newStore.id)).rejects.toHaveProperty("message", "The product with the given id was not found");
   });
 
+  it('findStoresFromProduct should return stores by product', async () => {
+    const stores: StoreEntity[] = await service.findStoresFromProduct(product.id);
+    expect(stores.length).toBe(5)
+  });
+
+  it('findStoresFromProduct should throw an exception for an invalid product', async () => {
+    await expect(() => service.findStoresFromProduct("0")).rejects.toHaveProperty("message", "The product with the given id was not found");
+  });
+
   it('findStoreFromProduct should return a store by product', async () => {
     const store: StoreEntity = storesList[0];
     const storedStore: StoreEntity = await service.findStoreFromProduct(product.id, store.id)
@@ -103,4 +112,77 @@ describe('ProductStoreService', () => {
     expect(storedStore.city).toBe(store.city);
     expect(storedStore.address).toBe(store.address);
   });
+
+  it('findStoreFromProduct should throw an exception for an invalid store', async () => {
+    await expect(() => service.findStoreFromProduct(product.id, "0")).rejects.toHaveProperty("message", "The store with the given id was not found");
+  });
+
+  it('findStoreFromProduct should throw an exception for an invalid product', async () => {
+    const store: StoreEntity = storesList[0];
+    await expect(() => service.findStoreFromProduct("0", store.id)).rejects.toHaveProperty("message", "The product with the given id was not found");
+  });
+
+  it('updateStoresFromProduct should update stores list for a product', async () => {
+    const newStore: StoreEntity = await storeRepository.save({
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress()
+    });
+
+    const updatedProduct: ProductEntity = await service.updateStoresFromProduct(product.id, [newStore]);
+    expect(updatedProduct.stores.length).toBe(1);
+
+    expect(updatedProduct.stores[0].name).toBe(newStore.name);
+    expect(updatedProduct.stores[0].city).toBe(newStore.city);
+    expect(updatedProduct.stores[0].address).toBe(newStore.address);
+  });
+
+  it('updateStoresFromProduct should throw an exception for an invalid product', async () => {
+    const newStore: StoreEntity = await storeRepository.save({
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress()
+    });
+
+    await expect(() => service.updateStoresFromProduct("0", [newStore])).rejects.toHaveProperty("message", "The product with the given id was not found");
+  });
+
+  it('updateStoresFromProduct should throw an exception for an invalid store', async () => {
+    const newStore: StoreEntity = storesList[0];
+    newStore.id = "0";
+
+    await expect(() => service.updateStoresFromProduct(product.id, [newStore])).rejects.toHaveProperty("message", "The store with the given id was not found");
+  });
+
+  it('deleteStoreFromProduct should remove an store from a product', async () => {
+    const store: StoreEntity = storesList[0];
+
+    await service.deleteStoreFromProduct(product.id, store.id);
+
+    const storedProduct: ProductEntity = await productRepository.findOne({ where: { id: product.id }, relations: ["stores"] });
+    const deletedStore: StoreEntity = storedProduct.stores.find(item => item.id === store.id);
+
+    expect(deletedStore).toBeUndefined();
+
+  });
+
+  it('deleteStoreFromProduct should thrown an exception for an invalid store', async () => {
+    await expect(() => service.deleteStoreFromProduct(product.id, "0")).rejects.toHaveProperty("message", "The store with the given id was not found");
+  });
+
+  it('deleteStoreFromProduct should thrown an exception for an invalid product', async () => {
+    const store: StoreEntity = storesList[0];
+    await expect(() => service.deleteStoreFromProduct("0", store.id)).rejects.toHaveProperty("message", "The product with the given id was not found");
+  });
+
+  it('deleteStoreFromProduct should thrown an exception for an non asocciated store', async () => {
+    const newStore: StoreEntity = await storeRepository.save({
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress()
+    });
+
+    await expect(() => service.deleteStoreFromProduct(product.id, newStore.id)).rejects.toHaveProperty("message", "The store with the given id is not associated to the product");
+  });
+
 });
