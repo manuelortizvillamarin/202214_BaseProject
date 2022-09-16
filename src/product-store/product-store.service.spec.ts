aeb5a88f473b9464daa9a190ac12a1e8017356da
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-16 00:46:19 -0500

    Add test for findStoreFromProduct

--- src/product-store/product-store.service.spec.ts
+++ src/product-store/product-store.service.spec.ts
@@ -53,4 +53,54 @@ describe('ProductStoreService', () => {
     expect(service).toBeDefined();
   });
 
+  it('addStoreToProduct should add a store to a product', async () => {
+    const newStore: StoreEntity = await storeRepository.save({
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress()
+    });
+
+    const newProduct: ProductEntity = await productRepository.save({
+      name: faker.company.name(),
+      price: faker.finance.amount(),
+      type: faker.lorem.word()
+    })
+
+    const result: ProductEntity = await service.addStoreToProduct(newProduct.id, newStore.id);
+
+    expect(result.stores.length).toBe(1);
+    expect(result.stores[0]).not.toBeNull();
+    expect(result.stores[0].name).toBe(newStore.name)
+    expect(result.stores[0].city).toBe(newStore.city)
+    expect(result.stores[0].address).toBe(newStore.address)
+  });
+
+  it('addStoreToProduct should thrown exception for an invalid store', async () => {
+    const newProduct: ProductEntity = await productRepository.save({
+      name: faker.company.name(),
+      price: faker.finance.amount(),
+      type: faker.lorem.word()
+    })
+
+    await expect(() => service.addStoreToProduct(newProduct.id, "0")).rejects.toHaveProperty("message", "The store with the given id was not found");
+  });
+
+  it('addStoreToProduct should throw an exception for an invalid product', async () => {
+    const newStore: StoreEntity = await storeRepository.save({
+      name: faker.company.name(),
+      city: faker.address.cityName(),
+      address: faker.address.streetAddress()
+    });
+
+    await expect(() => service.addStoreToProduct("0", newStore.id)).rejects.toHaveProperty("message", "The product with the given id was not found");
+  });
+
+  it('findStoreFromProduct should return a store by product', async () => {
+    const store: StoreEntity = storesList[0];
+    const storedStore: StoreEntity = await service.findStoreFromProduct(product.id, store.id)
+    expect(storedStore).not.toBeNull();
+    expect(storedStore.name).toBe(store.name);
+    expect(storedStore.city).toBe(store.city);
+    expect(storedStore.address).toBe(store.address);
+  });
 });
