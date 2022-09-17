eaa1de218ea9006b146011ffd6880eba6e57c01f
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-16 22:52:22 -0500

    Add controllers and DTOs

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
-import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
+import { StoreDto } from './store.dto';
+import { StoreEntity } from './store.entity';
 import { StoreService } from './store.service';
 
 @Controller('store')
@@ -8,4 +11,31 @@ export class StoreController {
 
     constructor(private readonly storeService: StoreService) { }
 
+    @Get()
+    async findAll() {
+        return await this.storeService.findAll();
+    }
+
+    @Get(':storeId')
+    async findOne(@Param('storeId') storeId: string) {
+        return await this.storeService.findOne(storeId);
+    }
+
+    @Post()
+    async create(@Body() storeDto: StoreDto) {
+        const store: StoreEntity = plainToInstance(StoreEntity, storeDto);
+        return await this.storeService.create(store);
+    }
+
+    @Put(':storeId')
+    async update(@Param('storeId') storeId: string, @Body() storeDto: StoreDto) {
+        const store: StoreEntity = plainToInstance(StoreEntity, storeDto);
+        return await this.storeService.update(storeId, store);
+    }
+
+    @Delete(':storeId')
+    @HttpCode(204)
+    async delete(@Param('storeId') storeId: string) {
+        return await this.storeService.delete(storeId);
+    }
 }
