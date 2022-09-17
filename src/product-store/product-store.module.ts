87136448d0225c59477b046d453d6933e25057b1
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2022-09-17 00:09:30 -0500

    Fix import in product-store module

--- src/product-store/product-store.module.ts
+++ src/product-store/product-store.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from '../product/product.entity';
 import { ProductStoreService } from './product-store.service';
 import { ProductStoreController } from './product-store.controller';
+import { StoreEntity } from '../store/store.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  imports: [TypeOrmModule.forFeature([ProductEntity, StoreEntity])],
   providers: [ProductStoreService],
   controllers: [ProductStoreController]
 })
